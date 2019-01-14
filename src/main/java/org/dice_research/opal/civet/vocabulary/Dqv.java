26c58413f3cceb4e0138cf2645c3892c140fd60b
Adrian Wilke <adibaba@users.noreply.github.com>
2019-01-14 17:19:33 +0100

    Added DQV terms

--- src/main/java/org/dice_research/opal/civet/vocabulary/Dqv.java
+++ src/main/java/org/dice_research/opal/civet/vocabulary/Dqv.java
@@ -13,10 +13,15 @@ public class Dqv {
 
 	public static final String NS = "http://www.w3.org/ns/dqv#";
 
-	public static final String IN_CATEGORY = NS + "inCategory";
-	public static final String IN_DIMENSION = NS + "inDimension";
+	public static final String HAS_QUALITY_MEASUREMENT = NS + "hasQualityMeasurement";
+	public static final String QUALITY_MEASUREMENT = NS + "QualityMeasurement";
 
-	public static final String CATEGORY = NS + "Category";
-	public static final String DIMENSION = NS + "Dimension";
+	public static final String IS_MEASUREMENT_OF = NS + "isMeasurementOf";
 	public static final String METRIC = NS + "Metric";
+
+	public static final String IN_DIMENSION = NS + "inDimension";
+	public static final String DIMENSION = NS + "Dimension";
+
+	public static final String IN_CATEGORY = NS + "inCategory";
+	public static final String CATEGORY = NS + "Category";
 }
