921614f7042dd04b48c251098f37df4dc2ebfc17
Adrian Wilke <adibaba@users.noreply.github.com>
2020-01-14 12:27:46 +0100

    UpdateRateMetric refactoring

--- src/main/java/org/dice_research/opal/civet/metrics/UpdateRateMetric.java
+++ src/main/java/org/dice_research/opal/civet/metrics/UpdateRateMetric.java
@@ -1,62 +1,118 @@
 package org.dice_research.opal.civet.metrics;
 
-import org.apache.jena.rdf.model.*;
-import org.apache.jena.vocabulary.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.vocabulary.DCTerms;
 import org.dice_research.opal.civet.Metric;
 import org.dice_research.opal.common.vocabulary.Opal;
 
 /**
- * The UpdateRateMetric awards stars based on how often the
- * dataset is updated
+ * The UpdateRateMetric awards stars based on how often the dataset is updated.
+ * 
+ * Values are based on the Dublin Core Collection Description Frequency
+ * Vocabulary as well on values defined by europa.eu.
+ * 
+ * Values of europa.eu: ANNUAL ANNUAL_2 ANNUAL_3 BIENNIAL BIMONTHLY BIWEEKLY
+ * CONT DAILY DAILY_2 IRREG MONTHLY MONTHLY_2 MONTHLY_3 NEVER OP_DATPRO
+ * QUARTERLY TRIENNIAL UNKNOWN UPDATE_CONT WEEKLY_2 WEEKLY_3 QUINQUENNIAL
+ * DECENNIAL HOURLY QUADRENNIAL BIHOURLY TRIHOURLY BIDECENNIAL TRIDECENNIAL
+ * 
+ * Values of dublincore.org: freq:triennial freq:biennial freq:annual
+ * freq:semiannual freq:threeTimesAYear freq:quarterly freq:bimonthly
+ * freq:monthly freq:semimonthly freq:biweekly freq:threeTimesAMonth freq:weekly
+ * freq:semiweekly freq:threeTimesAWeek freq:daily freq:continuous
+ * freq:irregular
+ * 
+ * @see https://www.w3.org/TR/vocab-dcat/#temporal-properties
+ * @see https://www.dublincore.org/specifications/dublin-core/collection-description/frequency/
+ * @see http://publications.europa.eu/resource/authority/frequency
+ * 
+ * @author Vikrant Singh, Adrian Wilke
  */
 
 public class UpdateRateMetric implements Metric {
 
-	private static final Logger LOGGER = LogManager.getLogger();
-	private static final String DESCRIPTION = "Computes the update rate/frequency"
-			+ "If data is updated WEEKLY, DAILY, HOURLY,"
-			+ "CONTINUOUS, CONT, 5 stars are awarded. "
-			+ "If data is updated MONTHLY, 4 stars are awarded."
-			+ "If data is updated QUARTERLY, 3 stars are awarded."
-			+ "If data is updated ANNUALLY,ANNUAL_2,TRIENNIAL"
-			+ "2 stars are awarded."
-			+ "If data is updated IRREGULARLY"
-			+ "1 star is awarded."
-			+ "FOR UNKNOWN ,null is returned. ";
+	private static final String DESCRIPTION = "Computes the update rate/frequency. "
+			+ "If data is updated at least weekly, 5 stars are awarded. "
+			+ "If data is updated at least monthly, 4 stars are awarded."
+			+ "If data is updated at least four times a year, 3 stars are awarded. "
+			+ "If data is updated at least once a year, 2 stars are awarded. "
+			+ "If data is updated sometimes, 1 star is awarded.";
+
+	protected static List<String> listUnknown = Arrays.asList(new String[] { "UNKNOWN" });
+
+	protected static List<String> listNo = Arrays.asList(new String[] { "OP_DATPRO", "NEVER" });
+
+	protected static List<String> listSometimes = Arrays.asList(new String[] { "IRREG", "BIENNIAL", "TRIENNIAL",
+			"DECENNIAL", "TRIDECENNIAL", "BIDECENNIAL", "QUINQUENNIAL" });
+
+	protected static List<String> listYearly = Arrays.asList(new String[] { "ANNUAL", "THREETIMESAYEAR" });
+
+	protected static List<String> listQuaterly = Arrays
+			.asList(new String[] { "QUARTERLY", "BIMONTHLY", "QUADRENNIAL" });
+
+	protected static List<String> listMonthly = Arrays
+			.asList(new String[] { "MONTHLY", "THREETIMESAMONTH", "BIWEEKLY" });
+
+	protected static List<String> listWeekly = Arrays.asList(
+			new String[] { "HOURLY", "WEEKLY", "CONT", "DAILY", "UPDATE_CONT", "THREETIMESAWEEK", "CONTINUOUS" });
 
 	@Override
 	public Integer compute(Model model, String datasetUri) throws Exception {
-
-		LOGGER.info("Processing dataset " + datasetUri);
-
 		Resource dataset = ResourceFactory.createResource(datasetUri);
 		Statement statement = model.getProperty(dataset, DCTerms.accrualPeriodicity);
-
-		String updateRate = "";
-		if(statement != null)
-			 updateRate = String.valueOf(statement.getObject()).toUpperCase();
-		else
+		String updateRate;
+		if (statement == null) {
 			return null;
+		} else {
+			updateRate = String.valueOf(statement.getObject()).toUpperCase();
+		}
 
-		     if(updateRate.contains("UNKNOWN"))
-			    return null;
-		else if (updateRate.contains("IRREG"))
-				return 1;
-		else if (updateRate.contains("TRIENNIAL") || updateRate.contains("ANNUAL_2")
-				|| updateRate.contains("ANNUAL"))
-				return 2;
-		else if(updateRate.contains("QUARTERLY"))
-				return 3;
-		else if (updateRate.contains("MONTHLY") || updateRate.contains("BIMONTHLY"))
-				return 4;
-		else if (updateRate.contains("WEEKLY") || updateRate.contains("DAILY")
-				|| updateRate.contains("HOURLY") || updateRate.contains("CONT")
-				|| updateRate.contains("BIWEEKLY"))
-				return 5;
-		else
-				return 0;
+		if (stringContainsListEntry(updateRate, listUnknown)) {
+			return null;
+		}
+
+		else if (stringContainsListEntry(updateRate, listNo)) {
+			return 0;
+		}
+
+		else if (stringContainsListEntry(updateRate, listSometimes)) {
+			return 1;
+		}
+
+		else if (stringContainsListEntry(updateRate, listYearly)) {
+			return 2;
+		}
+
+		else if (stringContainsListEntry(updateRate, listQuaterly)) {
+			return 3;
+		}
+
+		else if (stringContainsListEntry(updateRate, listMonthly)) {
+			return 4;
+		}
+
+		else if (stringContainsListEntry(updateRate, listWeekly)) {
+			return 5;
+		}
+
+		else {
+			return null;
+		}
+	}
+
+	protected boolean stringContainsListEntry(String string, List<String> list) {
+		for (String listString : list) {
+			if (string.contains(listString)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
--- src/test/java/org/dice_research/opal/civet/metrics/UpdateRateMetricTest.java
+++ src/test/java/org/dice_research/opal/civet/metrics/UpdateRateMetricTest.java
@@ -1,11 +1,6 @@
 package org.dice_research.opal.civet.metrics;
 
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.ResourceFactory;
-import org.apache.jena.vocabulary.DCAT;
-import org.apache.jena.vocabulary.RDF;
 import org.dice_research.opal.civet.TestData;
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,23 +12,23 @@ import org.junit.Test;
 public class UpdateRateMetricTest {
 
 	TestData testdata;
+	final String TEST_FILE = "TestDataUpdateRateMetric.ttl";
+	Model model;
 
 	@Before
 	public void setUp() throws Exception {
 		testdata = new TestData();
+		model = testdata.getModel(TEST_FILE);
 	}
-
 
 	@Test
 	public void testStarNull() throws Exception {
-		
-		// Compute stars
-		final String TEST_FILE = "TestData.ttl";
-		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-				"europeandataportal_eu_set_data_debat_dorientations_budgetaires_2019";
+
+		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___"
+				+ "europeandataportal_eu_set_data_debat_dorientations_budgetaires_2019";
 
 		UpdateRateMetric metric = new UpdateRateMetric();
-		Integer stars = metric.compute(testdata.getModel(TEST_FILE), TEST_DATASET);
+		Integer stars = metric.compute(model, TEST_DATASET);
 		Assert.assertNull(TEST_FILE, stars);
 
 	}
@@ -41,67 +36,57 @@ public class UpdateRateMetricTest {
 	@Test
 	public void testStar1() throws Exception {
 
-		final String TEST_FILE = "TestData.ttl";
-		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-				"europeandataportal_eu_set_data_declaration_du_profil_acheteur_35";
+		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___"
+				+ "europeandataportal_eu_set_data_declaration_du_profil_acheteur_35";
 
 		UpdateRateMetric metric = new UpdateRateMetric();
-		Integer stars = metric.compute(testdata.getModel(TEST_FILE), TEST_DATASET);
+		Integer stars = metric.compute(model, TEST_DATASET);
 		Assert.assertEquals(TEST_FILE, 1, stars.intValue());
 	}
-
 
 	@Test
 	public void testStar2() throws Exception {
 
-		final String TEST_FILE = "TestData.ttl";
-		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_" +
-				"defibrillateurs_presents_sur_la_commune_de_sixt_sur_aff_en_2018";
+		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___europeandataportal_eu_set_data_"
+				+ "defibrillateurs_presents_sur_la_commune_de_sixt_sur_aff_en_2018";
 
 		UpdateRateMetric metric = new UpdateRateMetric();
-		Integer stars = metric.compute(testdata.getModel(TEST_FILE), TEST_DATASET);
+		Integer stars = metric.compute(model, TEST_DATASET);
 		Assert.assertEquals(TEST_FILE, 2, stars.intValue());
 	}
-
 
 	@Test
 	public void testStar3() throws Exception {
 
-		final String TEST_FILE = "TestData.ttl";
-		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-				"europeandataportal_eu_set_data_de68f4fc_2886_4775_9537_2bf21d81dff7";
+		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___"
+				+ "europeandataportal_eu_set_data_de68f4fc_2886_4775_9537_2bf21d81dff7";
 
 		UpdateRateMetric metric = new UpdateRateMetric();
-		Integer stars = metric.compute(testdata.getModel(TEST_FILE), TEST_DATASET);
+		Integer stars = metric.compute(model, TEST_DATASET);
 		Assert.assertEquals(TEST_FILE, 3, stars.intValue());
 
 	}
 
-
 	@Test
 	public void testStar4() throws Exception {
 
-		final String TEST_FILE = "TestData.ttl";
-		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-				"europeandataportal_eu_set_data_ded2b43b_abec_49b1_9eb1_44d418ec7fb4";
+		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___"
+				+ "europeandataportal_eu_set_data_ded2b43b_abec_49b1_9eb1_44d418ec7fb4";
 
 		UpdateRateMetric metric = new UpdateRateMetric();
-		Integer stars = metric.compute(testdata.getModel(TEST_FILE), TEST_DATASET);
+		Integer stars = metric.compute(model, TEST_DATASET);
 		Assert.assertEquals(TEST_FILE, 4, stars.intValue());
 	}
-
 
 	@Test
 	public void testStar5() throws Exception {
 
-		final String TEST_FILE = "TestData.ttl";
-		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___" +
-				"europeandataportal_eu_set_data_de7414f5fdc7eee3_85c8205f_27eae5db_ff90930e_bd567a43a3521eb8b2250d88";
+		final String TEST_DATASET = "http://projekt-opal.de/dataset/https___"
+				+ "europeandataportal_eu_set_data_de7414f5fdc7eee3_85c8205f_27eae5db_ff90930e_bd567a43a3521eb8b2250d88";
 
 		UpdateRateMetric metric = new UpdateRateMetric();
-		Integer stars = metric.compute(testdata.getModel(TEST_FILE), TEST_DATASET);
+		Integer stars = metric.compute(model, TEST_DATASET);
 		Assert.assertEquals(TEST_FILE, 5, stars.intValue());
 	}
-
 
 }
