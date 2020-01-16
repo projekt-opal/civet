2126a74d14f232cef17cba76a5f262671bace526
Aamir Mohammed <maamir@mail.uni-paderborn.de>
2020-01-16 20:13:55 +0100

    feature-metric-date-format changes done

--- src/main/java/org/dice_research/opal/civet/metrics/Dateformat.java
+++ src/main/java/org/dice_research/opal/civet/metrics/Dateformat.java
@@ -16,8 +16,13 @@ import org.dice_research.opal.civet.Metric;
 import org.dice_research.opal.common.vocabulary.Opal;
 
 /**
- * The Dateformat metric awards stars based on the the dateformat in the dataset
- * 
+ * The Dateformat metric awards stars based on the the dateformat in the dataset.
+ * Here we are checking the predicate "dct:issued" in the dataset which contains 
+ * different formats of date.
+ * We have used the regex (Regular Expression) to check the correct date format.
+ * This is the format "\\d{4}-\\d{2}-\\d{2}" we are checking, if we get this type
+ * of date then we give 5 stars, else 1 star.
+ *  
  * @author Aamir Mohammed
  */
 public class Dateformat implements Metric {
@@ -27,12 +32,11 @@ public class Dateformat implements Metric {
 		} else {
 			return 1;
 		}
-
 	}
 
 	private static final Logger logger = LogManager.getLogger();
 	private static final String descriptions = "Check the metadata field of dateformat"
-			+ "If dateformat in the dataset is according to W3C standards then give 5 starts" + "Else return null";
+			+ "If dateformat in the predicate dct:issued in dataset is according to W3C standards then give 5 starts" + "Else return null";
 
 	@Override
 	public Integer compute(Model model, String datasetUri) throws Exception {
@@ -40,10 +44,11 @@ public class Dateformat implements Metric {
 		Resource dataset = ResourceFactory.createResource(datasetUri);
 
 		StmtIterator datasetIterator = model.listStatements(new SimpleSelector(dataset, RDF.type, DCAT.Dataset));
+		// Here we are running iterator in the dataset
 		if (datasetIterator.hasNext()) {
 			Statement dataSetSentence = datasetIterator.nextStatement();
 			Resource dataSet = dataSetSentence.getSubject();
-
+			// Here we are checking predicate dct:issued
 			if (dataSet.hasProperty(DCTerms.issued)
 					&& !(dataSet.getProperty(DCTerms.issued).getObject().toString().isEmpty())) {
 				String dateissued = dataSet.getProperty(DCTerms.issued).getObject().toString();
@@ -52,7 +57,6 @@ public class Dateformat implements Metric {
 			} else {
 				return null;
 			}
-
 		}
 		return null;
 	}
