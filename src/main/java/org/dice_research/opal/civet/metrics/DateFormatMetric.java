eb7fb55e0fd8d1609e3089e303e3537cd4b6b2be
Aamir Mohammed <maamir@mail.uni-paderborn.de>
2020-05-21 03:38:50 +0200

    Dateformat metric - Improved version

---
+++ src/main/java/org/dice_research/opal/civet/metrics/DateFormatMetric.java
@@ -0,0 +1,123 @@
+
+package org.dice_research.opal.civet.metrics;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.SimpleSelector;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.vocabulary.DCAT;
+import org.apache.jena.vocabulary.DCTerms;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dice_research.opal.civet.Metric;
+import org.dice_research.opal.common.vocabulary.Opal;
+
+/**
+ * For the Date Format metric, the following will be checked: Check, if the date
+ * format is according to the W3c standards or not.
+ * 
+ * According to DCAT https://www.w3.org/TR/vocab-dcat-2/#bib-datetime the
+ * correct formats are here: https://www.w3.org/TR/NOTE-datetime. All formats
+ * have to be checked.
+ * 
+ * Here the implemented code checks the turtle file whether the given date
+ * format is according to W3c standard or not, if it is of the standard format
+ * it gives 5 stars else 0 star.
+ * 
+ * Finally, an average of the following 4 cases has to be computed for the final
+ * score: dataset: issued + modified, distributions: issued + modified
+ * 
+ * @author Aamir Mohammed
+ */
+public class DateFormatMetric implements Metric {
+
+	static List<String> correctFormat = new ArrayList<String>();
+	static {
+		correctFormat.add("YYYY");
+		correctFormat.add("YYYY-MM");
+		correctFormat.add("YYYY-MM-DD");
+		correctFormat.add("YYYY-MM-DDThh:mmTZD");
+		correctFormat.add("YYYY-MM-DDThh:mm:ssTZD");
+		correctFormat.add("YYYY-MM-DDThh:mm:ss.sTZD");
+	}
+
+	public static int checkDateFormat(String dct_issued) {
+		for (String string : correctFormat) {
+			SimpleDateFormat Format = new SimpleDateFormat(string);
+			try {
+				Format.parse(dct_issued.trim());
+				return 5;
+
+			} catch (ParseException pe) {
+				continue;
+			}
+		}
+		return 0;
+	}
+
+	private static final Logger LOGGER = LogManager.getLogger();
+	private static final String DESCRIPTIONS = "If the date format is in the correct format then give 5 stars else 0 star"
+			+ "Finally, an average of the following 4 cases has to be computed for the finalscore: dataset: issued + modified, distributions: issued + modified";
+
+	@Override
+	public Integer compute(Model model, String datasetUri) throws Exception {
+		int result = 0;
+		LOGGER.info("Processing dataset " + datasetUri);
+		Resource dataSet = model.createResource(datasetUri);
+		if (dataSet.hasProperty(DCTerms.issued)
+				&& !(dataSet.getProperty(DCTerms.issued).getObject().toString().isEmpty())) {
+			String dct_issued = dataSet.getProperty(DCTerms.issued).getObject().toString();
+			result += checkDateFormat(dct_issued);
+		} else {
+			result += 0;
+		}
+		if (dataSet.hasProperty(DCTerms.modified)
+				&& !(dataSet.getProperty(DCTerms.modified).getObject().toString().isEmpty())) {
+			String dateissued = dataSet.getProperty(DCTerms.modified).getObject().toString();
+			result += checkDateFormat(dateissued);
+		} else {
+			result += 0;
+		}
+		int countDistributions = 0;
+		StmtIterator distribution = model
+				.listStatements(new SimpleSelector(dataSet, DCAT.distribution, (RDFNode) null));
+		while (distribution.hasNext()) {
+			Statement distSetSentence = distribution.nextStatement();
+			Resource dist = distSetSentence.getObject().asResource();
+			if (dist.hasProperty(DCTerms.issued)
+					&& !(dist.getProperty(DCTerms.issued).getObject().toString().isEmpty())) {
+				String dateissued = dist.getProperty(DCTerms.issued).getObject().toString();
+				result += checkDateFormat(dateissued);
+			} else {
+				result += 0;
+			}
+			if (dist.hasProperty(DCTerms.modified)
+					&& !(dist.getProperty(DCTerms.modified).getObject().toString().isEmpty())) {
+				String dateissued = dist.getProperty(DCTerms.modified).getObject().toString();
+				result += checkDateFormat(dateissued);
+			} else {
+				result += 0;
+			}
+			countDistributions++;
+		}
+		return result / (2 * countDistributions + 2);
+	}
+
+	@Override
+	public String getDescription() {
+		return DESCRIPTIONS;
+	}
+
+	@Override
+	public String getUri() throws Exception {
+		return Opal.OPAL_METRIC_CATEGORIZATION.getURI();
+	}
+
+}
---
+++ src/test/java/org/dice_research/opal/civet/metrics/DateFormatMetricTest.java
@@ -0,0 +1,84 @@
+package org.dice_research.opal.civet.metrics;
+
+import org.apache.jena.ext.com.google.common.annotations.VisibleForTesting;
+import org.apache.jena.rdf.model.Model;
+import org.dice_research.opal.civet.TestData;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests {@link Dateformat}.
+ * 
+ * @author Aamir Mohammed
+ */
+public class DateFormatMetricTest {
+
+	@VisibleForTesting
+	TestData testdata;
+	@VisibleForTesting
+	final String MODEL = "TestDateFormatMetric.ttl";
+	@VisibleForTesting
+	Model model;
+
+	@Before
+	public void setUp() throws Exception {
+		testdata = new TestData();
+		model = testdata.getModel(MODEL);
+	}
+
+	@Test
+	public void correctFormat() throws Exception {
+
+		DateFormatMetric metric = new DateFormatMetric();
+		final String datasetUri = "http://projekt-opal.de/dataset/_mcloudde_standortederdauerzhlstellenradverkehrindsseldorf";
+		Integer stars = metric.compute(model, datasetUri);
+		Assert.assertEquals("It contains all correct format", 5, stars.intValue());
+	}
+
+	@Test
+	public void mostCorrectFormat() throws Exception {
+
+		DateFormatMetric metric = new DateFormatMetric();
+		final String datasetUri = "http://projekt-opal.de/dataset/_mcloudde_vieljhrlicherasterdesmittlerenvegetationsbeginnsindeutschland";
+		Integer stars = metric.compute(model, datasetUri);
+		Assert.assertEquals("It contains almost correct format", 4, stars.intValue());
+	}
+
+	@Test
+	public void someCorrectFormat() throws Exception {
+
+		DateFormatMetric metric = new DateFormatMetric();
+		final String datasetUri = "http://projekt-opal.de/dataset/_mcloudde_gerastertetglichemittlerebodennahelufttemperaturfreuropaprojektdecregmiklip-versionv001abgelstever";
+		Integer stars = metric.compute(model, datasetUri);
+		Assert.assertEquals("It contains half correct format", 3, stars.intValue());
+	}
+
+	@Test
+	public void lessCorrectFormat() throws Exception {
+
+		DateFormatMetric metric = new DateFormatMetric();
+		final String datasetUri = "http://projekt-opal.de/dataset/_mcloudde_gerastertestglichesminimumderbodennahenlufttemperaturfreuropaprojektdecregmiklip-versionv001abgels";
+		Integer stars = metric.compute(model, datasetUri);
+		Assert.assertEquals("It contains less correct format", 2, stars.intValue());
+	}
+
+	@Test
+	public void oneCorrectFormat() throws Exception {
+
+		DateFormatMetric metric = new DateFormatMetric();
+		final String datasetUri = "http://projekt-opal.de/dataset/_mcloudde_gezeiten-undwindstaukurvenfr16pegelinderdeutschenbuchtmintlich";
+		Integer stars = metric.compute(model, datasetUri);
+		Assert.assertEquals("It contains one correct format", 1, stars.intValue());
+	}
+
+	@Test
+	public void noCorrectFormat() throws Exception {
+
+		DateFormatMetric metric = new DateFormatMetric();
+		final String datasetUri = "http://projekt-opal.de/dataset/_mcloudde_schienennetzdichteingebietsflche";
+		Integer stars = metric.compute(model, datasetUri);
+		Assert.assertEquals("It contains no correct format", 0, stars.intValue());
+	}
+
+}
